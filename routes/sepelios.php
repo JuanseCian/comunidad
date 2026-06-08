088c593e3bc94a581d84a4ca29f73e36108e5422
ChVilchez <87630718+ChVilchez@users.noreply.github.com>
2026-06-08 09:58:30 -0300

    rutas

--- routes/sepelios.php
+++ routes/sepelios.php
@@ -1,36 +1,26 @@
 <?php
 
+use App\Http\Controllers\frontend\SepelioController;
+
 Route::prefix('recepcion/sepelios')
     ->name('recepcion.sepelios.')
     ->group(function () {
 
-        Route::get('/', [
-            \App\Http\Controllers\Frontend\SepelioController::class,
-            'index'
-        ])->name('index');
+        Route::get('/', [SepelioController::class, 'index'])
+            ->name('index');
 
-        Route::get('/create', [
-            \App\Http\Controllers\Frontend\SepelioController::class,
-            'create'
-        ])->name('create');
+        Route::get('/create', [SepelioController::class, 'create'])
+            ->name('create');
 
-        Route::post('/', [
-            \App\Http\Controllers\Frontend\SepelioController::class,
-            'store'
-        ])->name('store');
+        Route::post('/', [SepelioController::class, 'store'])
+            ->name('store');
 
-        Route::get('/buscar-personas', [
-            \App\Http\Controllers\Frontend\SepelioController::class,
-            'buscarPersonas'
-        ])->name('buscar-personas');
+        Route::get('/buscar-personas', [SepelioController::class, 'buscarPersonas'])
+            ->name('buscar-personas');
 
-        Route::get('/{sepelio}', [
-            \App\Http\Controllers\Frontend\SepelioController::class,
-            'show'
-        ])->name('show');
+        Route::get('/{sepelio}', [SepelioController::class, 'show'])
+            ->name('show');
 
-        Route::delete('/{sepelio}', [
-            \App\Http\Controllers\Frontend\SepelioController::class,
-            'destroy'
-        ])->name('destroy');
+        Route::delete('/{sepelio}', [SepelioController::class, 'destroy'])
+            ->name('destroy');
     });
